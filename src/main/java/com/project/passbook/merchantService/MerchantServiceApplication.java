b884d365eeb277bbd9459eb64ce6293e7c448b88
YunchuWang <yunchuwang5@gmail.com>
2020-04-11 12:21:19 -0700

    update git ignore

--- src/main/java/com/project/passbook/merchantService/MerchantServiceApplication.java
+++ src/main/java/com/project/passbook/merchantService/MerchantServiceApplication.java
@@ -1,18 +1,27 @@
 package com.project.passbook.merchantService;
 
 import com.project.passbook.merchantService.security.AuthenticationInterceptor;
+import java.util.List;
 import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableJpaRepositories(basePackages = { "com.project.passbook.merchantService" })
-@ComponentScan(basePackages = { "com.project.passbook.merchantService" })
-@EntityScan(basePackages = { "com.project.passbook.merchantService" })
+@EnableDiscoveryClient
+@EnableJpaRepositories(basePackages = {"com.project.passbook.merchantService"})
+@ComponentScan(basePackages = {"com.project.passbook.merchantService"})
+@EntityScan(basePackages = {"com.project.passbook.merchantService"})
 @SpringBootApplication
 public class MerchantServiceApplication implements WebMvcConfigurer {
 
@@ -25,6 +34,19 @@ public class MerchantServiceApplication implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(authenticationInterceptor).addPathPatterns("/**");
+    registry.addInterceptor(authenticationInterceptor)
+            .addPathPatterns("/**");
+  }
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
+  @RequestMapping("/service-instances/{applicationName}")
+  public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+    return this.discoveryClient.getInstances(applicationName);
   }
 }
